a3dc4f77f50bcf558d55e8a2054e399a604dc0bc
bcapuano <bcapuano@asu.edu>
2022-10-10 15:26:38 -0400

    Caught leaks

--- mayhem/fuzz_read.py
+++ mayhem/fuzz_read.py
@@ -11,12 +11,9 @@ logging.disable(logging.CRITICAL)
 with atheris.instrument_imports():
     import pyreadr
 
-# f = tempfile.NamedTemporaryFile()
 
 @atheris.instrument_func
 def TestOneInput(data):
-    # f.write(data)
-    # f.flush()
     try:
         with tempfile.NamedTemporaryFile() as f:
             f.write(data)
@@ -24,9 +21,6 @@ def TestOneInput(data):
             pyreadr.read_r(f.name)
     except (PyreadrError, LibrdataError):
         pass
-    # f.seek(0)
-    # f.truncate()
-    # f.flush()
 
 def main():
     atheris.Setup(sys.argv, TestOneInput)
