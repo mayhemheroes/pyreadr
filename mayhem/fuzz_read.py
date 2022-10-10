85ff149048dfc9b5d783675a0af21066847f8124
bcapuano <bcapuano@asu.edu>
2022-10-10 16:08:44 -0400

    Memory leak handling

--- mayhem/fuzz_read.py
+++ mayhem/fuzz_read.py
@@ -2,6 +2,7 @@
 import logging
 import sys
 import tempfile
+import gc
 
 import atheris
 from pyreadr import PyreadrError, LibrdataError
@@ -12,13 +13,27 @@ with atheris.instrument_imports():
     import pyreadr
 
 
+EXECS_SINCE_GC = 0
+MAX_EXECS_SINCE_GC = 1000
+
+f = tempfile.NamedTemporaryFile()
+
 @atheris.instrument_func
 def TestOneInput(data):
+    global EXECS_SINCE_GC
+
+    ## The underlying codebase is riddled with memory leaks, this is my attempt to resolve that
+    EXECS_SINCE_GC += 1
+    if EXECS_SINCE_GC > MAX_EXECS_SINCE_GC:
+        gc.collect()
+        EXECS_SINCE_GC = 0
+
     try:
-        with tempfile.NamedTemporaryFile() as f:
-            f.write(data)
-            f.flush()
-            pyreadr.read_r(f.name)
+        f.seek(0)
+        f.truncate()
+        f.write(data)
+        f.flush()
+        pyreadr.read_r(f.name)
     except (PyreadrError, LibrdataError):
         pass
 
